5abc56da80dc566bc5d477948544154d0deabbc6
Blake Smith <xxblakefailxx@gmail.com>
2014-04-26 18:24:18 -0400

    added put and post responses

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,19 +33,50 @@ tripLog.config(['$provide', function ($routeProvider, $provide) {
     });
 });
 
-tripLog.run(['$httpBackend', 'Datafaker', '$rootScope', 'ENV', function($httpBackend, Datafaker, $rootScope, ENV) {
+tripLog.run(['$httpBackend', 'DataFaker', '$rootScope', 'ENV', function($httpBackend, DataFaker, $rootScope, ENV) {
   if(ENV === 'development') {
     $httpBackend.whenGet('/trips').respond();
     $httpBackend.whenGet(/trips\/[0-9]*/).respond();
     $httpBackend.whenGet(/trips\/[0-9]*\/photos/).respond();
 
-    $httpBackend.whenPost('/trips').respond();
-    $httpBackend.whenPost(/trips\/[0-9]*\/photos/)
+    function generateID() { 
+      var uniqueID;
+      var date = new Date();
+      var components = [
+        date.getYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+        date.getMilliseconds()
+      ];
+      uniqueID = parseInt(components.join(""));
+      uniqueID = (uniqueID * ((1 + Math.random()) * 0x10000)).toString(16); 
+      return uniqueID;
+    }
+
+    $httpBackend.whenPost('/trips').respond(function(method, url, data) {
+      var trip = JSON.parse(data);
+      trip.id = generateID();
+      return [200, trip];
+    });
+    $httpBackend.whenPost(/trips\/[0-9]*\/photos/).respond(function(method, url, data) {
+      var photo = JSON.parse(data);
+      photo.id = generateID;
+      return [200, photo];
+    });
 
     $httpBackend.whenDelete(/trips\/[0-9]*/).respond(200, "The trip has been removed.");
     $httpBackend.whenDelete(/trips\/[0-9]*\/photos\/[0-9]*/).respond(200, "The photo has been removed.");
 
-    $httpBackend.whenPut(/trips\/[0-9]*/).respond();
-    $httpBackend.whenPut(/trips\/[0-9]*\/photos\/[0-9]*/).respond();
+    $httpBackend.whenPut(/trips\/[0-9]*/).respond(function(method, url, data) {
+      var trip = JSON.parse(data);
+      return [200, trip];
+    });
+    $httpBackend.whenPut(/trips\/[0-9]*\/photos\/[0-9]*/).respond(function(method, url, data) {
+      var photo = JSON.parse(data);
+      return [200, photo];
+    });
   }
 }])
