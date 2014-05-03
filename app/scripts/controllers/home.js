8983ecd5b2b3d7b55490c7cb7de0a63988e0ae98
Blake Smith <xxblakefailxx@gmail.com>
2014-05-03 14:14:57 -0400

    more fixes

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('triplog')
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl',['$http', '$scope', function ($http, $scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    $http.get('/trips/1/photos').success(function(res) {
+    	console.log(res);
+    })
+  }]);
